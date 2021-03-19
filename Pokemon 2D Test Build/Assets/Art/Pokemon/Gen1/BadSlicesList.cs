0f2b49439904e08baed76e6409c604a6981d5d19
EricBeaulieu <ebeaulieu5@gmail.com>
2021-03-19 13:39:31 -0400

    Sliced Gen 1
    Sliced to the third row for pokemon, named and updated

--- Pokemon 2D Test Build/Assets/Art/Pokemon/Gen1/BadSlicesList.cs
+++ Pokemon 2D Test Build/Assets/Art/Pokemon/Gen1/BadSlicesList.cs
@@ -6,4 +6,7 @@ public class BadSlicesList : MonoBehaviour
 {
 //004 all back sprites
 //007 back B sprites, bottom part of shell cut off
+//013 back A sprites, bottom 2 pixels are all cut off
+//017 Back B sprites, bottom 2 pixels are all cut off
+//022 Ferrow Back Sprites, Bottom pixel all cut off
 }
